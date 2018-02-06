fb0f599207b5a9133f29a3e2a201baa564a311d0
Tanya Saroha <tanya@joshsoftware.com>
2018-02-06 16:09:20 +0530

    [Tanya] Added Validations of attributes

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,7 @@
 class Message < ApplicationRecord
+	validates :content, presence: true, length: {maximum: 1200, messgae: 'Message length should be less than 1200 characters'}
+	validates :receiver_id, presence: true, numeracality: {only_integer: true, greater_than: 0, message: 'Should be an integer greater than 0'}
+	validates :is_read, inclusion: { in: [1, 0] }
+	validates :is_liked, inclusion: { in: [1, 0] }
+	validates :is_unlocked, inclusion: { in: [1, 0] }	
 end
