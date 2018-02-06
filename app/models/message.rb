b5c0c3c52777e6cf7c379541dc3a95c553b13f94
Tanya Saroha <tanya@joshsoftware.com>
2018-02-06 16:11:34 +0530

    [Tanya] Added association with user

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ApplicationRecord
+	belongs_to :user
 	validates :content, presence: true, length: {maximum: 1200, messgae: 'Message length should be less than 1200 characters'}
 	validates :receiver_id, presence: true, numeracality: {only_integer: true, greater_than: 0, message: 'Should be an integer greater than 0'}
 	validates :is_read, inclusion: { in: [1, 0] }
