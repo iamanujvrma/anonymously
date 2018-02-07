08f8b6f1a300647b686fd13945cf9ebc4de0c5b8
pinky <pinkyrout10@gmail.com>
2018-02-07 21:17:15 +0530

    user name validations and few test cases added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name, presence: true,
+                   uniqueness: true,
+                   length: { minimum: 2, maximum: 20 },
+	                 format: {
+	                 	 with: /\A[A-Za-z][A-Za-z0-9]*\s*_*[A-Za-z0-9]*/,
+	                   message: "name cannot start with a number or underscore"
+	                 }
 end
