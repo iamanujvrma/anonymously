e1eda66a9614bd48e1450d61f8b766a6f91abafe
the-spectator <mailakshayb1@gmail.com>
2018-02-07 21:37:13 +0530

    [akshay]required factorybot gem correctly in rails_helper

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,24 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
-
-Rspec.describe User do
-	let(:user) { build(:user, name: 'pinkyrout')}
+RSpec.describe User do
+  let(:user) { build(:user, name: 'pinkyrout')}
   let(:user1) { build(:user, name: '12pinky')}
-	
-	context 'check validity of name' do
-		
-		it 'validates name with only alphabets' do
-	    expect(user.valid?).to eq(true)
-	  end
+
+  context 'check validity of name' do
+
+    it 'validates name with only alphabets' do
+      expect(user.valid?).to eq(true)
+    end
 
     it 'validates name starting with a number' do
-    	expect(user1.valid?).to eq(false)
-    	expect(user1.errors['name']).to eq(["name cannot start with a number or underscore"])
+      expect(user1.valid?).to eq(false)
+      expect(user1.errors['name']).to eq(["name cannot start with a number or underscore"])
     end
-    	
-	end
-	
+
+  end
+
 end
