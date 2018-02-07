1868af5ca27e2f008342dbb735fd390bb8510407
pegasusflyhigh <bbhuvnaprabhu@gmail.com>
2018-02-07 23:30:31 +0530

    [bhuvna]validations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :gender , presence: true 
+	validates_inclusion_of :gender, :in => ['Male','male','Female','female']
 end
--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,3 +1,4 @@
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
+
 end
