02ec9b39927bdfb4d604fe22561f0ab7784f5ba3
Tanya Saroha <tanya@joshsoftware.com>
2018-02-08 13:24:09 +0530

    Correcting indentation of files

--- db/migrate/20180206094718_add_columns_to_message.rb
+++ db/migrate/20180206094718_add_columns_to_message.rb
@@ -1,9 +1,9 @@
 class AddColumnsToMessage < ActiveRecord::Migration[5.1]
-  def change
-    add_column :messages, :receiver_id, :integer
-    add_column :messages, :content, :text
-    add_column :messages, :is_liked, :integer
-    add_column :messages, :is_unlocked, :integer
-    add_column :messages, :is_read, :integer
-  end
+	def change
+		add_column :messages, :receiver_id, :integer
+		add_column :messages, :content, :text
+		add_column :messages, :is_liked, :integer
+		add_column :messages, :is_unlocked, :integer
+		add_column :messages, :is_read, :integer
+	end
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+
 end
 
 
