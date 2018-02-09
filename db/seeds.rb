56a23501ccfd8f7bed83cb0cd5870cfb4c10d5f0
the-spectator <mailakshayb1@gmail.com>
2018-02-09 11:54:24 +0530

    [akshay]Updated speeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'faker'
-10.times do 
-  User.create do |user| 
-    user.name = Faker::Name.name
-    user.gender = 'Male'
-    user.email = Faker::Internet.email
-    user.date_of_birth =  Faker::Date.birthday
-    user.password =	 Faker::Internet.password
-  end
-end
+
+User.create(name: 'Boring', email: 'boring25@company.com', password: 'boringcompany', gender: 'Female', date_of_birth: '1996-02-09 06:15:20')
+User.create(name: 'Elon', email: 'elon@musk.com', password: 'teslaislife', gender: 'Male', date_of_birth: '1980-02-09 12:50:20')
+User.create(name: 'Solarcity', email: 'solar@city.com', password: 'solarisfuture', gender: 'Female', date_of_birth: '2008-02-09 08:05:20')
+User.create(name: 'Starman', email: 'starman@spacex.com', password: 'roadsterisawesome', gender: 'Male', date_of_birth: '2018-02-05 01:15:20')
+
