2e75227d27e0f86a21ccff2230fe8bc86d97318c
the-spectator <mailakshayb1@gmail.com>
2018-02-12 12:55:04 +0530

    [akshay]Corrected rubocop offences

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,41 +1,37 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-  validates :gender , presence: true 
-	validates_inclusion_of :gender, :in => ['Male','male','Female','female'], message: 'Invalid gender'
-
+  validates :gender, presence: true
+  validates_inclusion_of :gender, in: %w[Male male Female female], message: 'Invalid gender'
 
   validates :name, presence: true,
                    length: { minimum: 2, maximum: 20 },
                    format: {
-                            with: /\A[A-Za-z][A-Za-z0-9]*\s*_*[A-Za-z0-9]*/,
-                            message: "name cannot start with a number or underscore"
-                           }
- 
+                     with: /\A[A-Za-z][A-Za-z0-9]*\s*_*[A-Za-z0-9]*/,
+                     message: 'name cannot start with a number or underscore'
+                   }
 
   validates :date_of_birth, presence: true
   validate :valid_dob?
-  
+
   has_many :messages
 
   private
+
   def valid_dob?
-    if valid_date?
-      if date_of_birth.future?
-        errors.add(:date_of_birth, "Date of Birth cannot be in future.")
-      end
-    end
+    return unless valid_date?
+    return unless date_of_birth.future?
+    errors.add(:date_of_birth, 'Date of Birth cannot be in future.')
   end
 
   def valid_date?
-    if date_of_birth.is_a?(Time)
-      return true
-    end
-    errors.add(:date_of_birth, "Is an invalid date.")
+    return true if date_of_birth.is_a?(Time)
+    errors.add(:date_of_birth, 'Is an invalid date.')
     false
   end
-
 end
