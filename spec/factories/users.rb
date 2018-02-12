993a25d871c1c2ab93b7ac4d2cabd8ebeba75737
pinky <pinkyrout10@gmail.com>
2018-02-12 10:44:14 +0530

    [pinky] more test cases added

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     name "MyString"
     date_of_birth "2018-02-06 16:21:16"
-    gender "female"
+    gender "male"
     email "pinkyrout10@gmail.com"
     password "pinky@10"
   end
