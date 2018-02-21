212b85c4d450536c2a9e5f70b03fa7791bab9e74
Tanya Saroha <tanya@joshsoftware.com>
2018-02-22 00:26:56 +0530

    [Tanya] Added sender and receiver associations in message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ApplicationRecord
-  belongs_to :user
+  belongs_to :sender, class_name: "User", foreign_key: "sender_id"
+  belongs_to :receiver, class_name: "User", foreign_key: "receiver_id"
   validates :content, presence: true, length: {maximum: 1200, messgae: 'Message length should be less than 1200 characters'}
-  validates :receiver_id, presence: true, numeracality: {only_integer: true, greater_than: 0, message: 'Should be an integer greater than 0'}
   validates :is_read, inclusion: { in: [1, 0] }
   validates :is_liked, inclusion: { in: [1, 0] }
   validates :is_unlocked, inclusion: { in: [1, 0] }   
