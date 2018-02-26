7463af17a2425f2165442f95e3e0b3997aefacd7
priyanka yadav <priyanka216668@gmail.com>
2018-02-26 18:03:22 +0530

    [Priyanka] empty file

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,24 +1,4 @@
 FactoryBot.define do
-  factory :message do
-    trait :message_present do
-      content 'This is a Message Text'
-    end
 
-    trait :message_empty do
-      content nil
-    end
-
-    trait :receiver_id_found do
-      receiver_id 101
-    end
-
-    trait :receiver_id_not_found do
-      receiver_id nil
-    end
-		
-    factory :message_given_for_valid_user, traits: [:message_present, :receiver_id_found]
-    factory :no_message_given_for_valid_user, traits: [:message_empty, :receiver_id_found]
-    factory :no_message_given_for_invalid_user, traits: [:message_empty, :receiver_id_not_found]
-    factory :message_given_for_invalid_user, traits: [:message_present, :receiver_id_not_found]
   end
 end
