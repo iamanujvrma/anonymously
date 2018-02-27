d32a1fe1c3cee249c18bff8f2b0ee7cda7ea7204
priyanka yadav <priyanka216668@gmail.com>
2018-02-27 11:44:37 +0530

    [Priyanka] Performed testcase for Message model association

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Message, type: :model do
   #pending "add some examples to (or delete) #{__FILE__}"
+=begin
   context 'Checking Content Validity' do
-=begin    
+    
     #let(:message1) { build(:message) }
     #let(:message2) { build(:message) }
     #let(:message3) { build(:message, content: 'Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.Hello, this is sample text.') }
@@ -13,10 +14,16 @@ RSpec.describe Message, type: :model do
     it 'Checks that message does not exceed 1200 characters' do
       should validate_length_of(:content).is_at_most(1200)
     end
-=end
+
     let(:message2) { build(:message) }
     it 'Validates a valid message' do
       expect(message2).to be_valid
     end
-  end 	
+  end 
+=end
+  context 'Checking Message Associations' do
+    it { should belong_to(:sender) }
+    it { should belong_to(:receiver) }
+  end
+
 end
