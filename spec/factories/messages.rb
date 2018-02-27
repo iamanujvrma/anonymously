7db2d9868095d2af3169136a07cd27a8713f6ba5
priyanka yadav <priyanka216668@gmail.com>
2018-02-27 11:44:37 +0530

    [Priyanka] Performed testcase for Message model association

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :message do
   content "Hello, Welcome to the app!!"
-  sender_id 101
-  receiver_id 102
+  association :sender, factory: :user
+  association :reciever, factory: :user
   end
 end
