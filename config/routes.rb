865a53f0ab02deeec854ebedf2cb31adfbdb90eb
pinky <pinkyrout10@gmail.com>
2018-02-28 23:01:58 +0530

    [pinky added default controllers for user and configured routes for start page]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  #devise_for :users, controllers: { sessions: 'users/sessions' }
+  #resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #root 'shared#_links'
+devise_for :users
+devise_scope :user do
+  root to: "devise/sessions#new"
+end
+ 
 end
