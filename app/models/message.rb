7881749f406e5bfb94a816ac6eb71e8ca6f125bb
TanyaSaroha <tanyasaroha@gmail.com>
2018-03-17 22:36:53 +0530

    [Tanya] removed maximum message length validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :sender, class_name: 'User', foreign_key: 'sender_id'
   belongs_to :receiver, class_name: 'User', foreign_key: 'receiver_id'
-  validates :content, presence: true, length: { maximum: 1200, messgae: 'Message length should be less than 1200 characters' }
+  validates :content, presence: true
   validates :is_read, inclusion: { in: [1, 0] }
   validates :is_liked, inclusion: { in: [1, 0] }
   validates :is_unlocked, inclusion: { in: [1, 0] }
