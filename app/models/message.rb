52ef19e580f69e3b8122ead136c23122df2c053a
priyanka yadav <priyanka216668@gmail.com>
2018-03-17 23:33:02 +0530

    [Priyanka] test cases for receiver_id should not be same as sender_id written

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,4 +5,11 @@ class Message < ApplicationRecord
   validates :is_read, inclusion: { in: [1, 0] }
   validates :is_liked, inclusion: { in: [1, 0] }
   validates :is_unlocked, inclusion: { in: [1, 0] }
+  validate  :sender_id_not_equal_to_receiver_id? 
+
+  def sender_id_not_equal_to_receiver_id?
+    if sender_id == receiver_id
+      self.errors.add(:base, 'sender id cannot be same as received id')
+    end
+  end
 end
