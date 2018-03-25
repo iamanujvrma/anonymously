6eecad3cf3606d414c3d3d736dfe072218256db1
dev <mailakshayb1@gmail.com>
2018-03-25 23:48:29 +0530

    [akshay]adds name,dob and gender in invitable form & edit in invitable locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :gender, :date_of_birth ])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :gender, :date_of_birth, :avatar, :avatar_cache, :remove_avatar])
+    devise_parameter_sanitizer.permit(:accept_invitation, keys: [:name, :gender, :date_of_birth ])
   end
 
   def after_sign_in_path_for(resource)
