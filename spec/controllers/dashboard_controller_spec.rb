6ae8c056119e887daedd11e4dd4312e3edef4fea
dev <mailakshayb1@gmail.com>
2018-04-01 19:24:20 +0530

    [akshay]Dashboard rsec testcases

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -3,10 +3,18 @@ require 'rails_helper'
 RSpec.describe DashboardController, type: :controller do
 
   describe "GET #index" do
+    it "blocks unauthenticated access" do
+      sign_in nil
+      get :index
+      expect(response).to redirect_to(new_user_session_path)
+    end
+
     it "returns http success" do
+      sign_in
       get :index
       expect(response).to have_http_status(:success)
     end
   end
+  
 
 end
