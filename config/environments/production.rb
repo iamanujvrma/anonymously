91b2e1e4d19ba21a310af68b643ca790f4b217c1
pinky <pinkyrout10@gmail.com>
2018-04-09 14:24:00 +0530

    [akshay]added gmail smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,20 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "anonymously_#{Rails.env}"
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.perform_caching = false
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            'tonyo0411@gmail.com',
+  password:             '$ROcky82@',
+  authentication:       :plain,
+  enable_starttls_auto: true
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
