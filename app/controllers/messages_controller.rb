a3db20e7b09a16c4aa861b76ae27616d0960607f
priyanka yadav <priyanka216668@gmail.com>
2018-04-09 13:06:59 +0530

    [Priyanka] controller for messages

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,27 @@
+class MessagesController < ApplicationController
+  def index
+  end
+  
+  def new
+  	@message = current_user.sent_messages.new()
+  end
+
+  def create
+    m1 = message_params
+    @email = m1[:receiver_id]
+    m1[:receiver_id] = User.find_by_email(@email).id
+    @message = current_user.sent_messages.new(m1)
+    @message.save!
+    if @message.save!
+      redirect_to dashboard_index_path
+    else
+      redirect_to new_message_path(current_user)
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:receiver_id, :content)
+ 	end
+end
