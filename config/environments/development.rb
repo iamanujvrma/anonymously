527cc3d029e26487497238c06cc435a8632bae26
dev <mailakshayb1@gmail.com>
2018-04-09 22:45:50 +0530

    [akshay]some smtp changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,8 +36,8 @@ Rails.application.configure do
   address:              'smtp.gmail.com',
   port:                 587,
   domain:               'gmail.com',
-  user_name:            'tonyo0411@gmail.com',
-  password:             '$ROcky82@',
+  user_name:            'freakyankit11@gmail.com',
+  password:             'nagnathbirajdar',
   authentication:       :plain,
   enable_starttls_auto: true
 }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,14 +64,14 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'anonymously-josh.herokuapp.com', port: 3000 }
   config.action_mailer.perform_caching = false
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
   domain:               'gmail.com',
-  user_name:            'tonyo0411@gmail.com',
-  password:             '$ROcky82@',
+  user_name:            'freakyankit11@gmail.com',
+  password:             'nagnathbirajdar',
   authentication:       :plain,
   enable_starttls_auto: true
   }
