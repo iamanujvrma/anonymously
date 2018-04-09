6e5cec79439e08bfc9417c41a9da42c4beb884d7
priyanka yadav <priyanka216668@gmail.com>
2018-04-09 13:10:23 +0530

    [Tanya] dashboard controller created

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,5 +3,27 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
   def index
+    @messages = current_user.sent_messages.all
+    @messages.each do |msg|
+      print msg.content
+    end
+  end
+
+=begin
+
+=end
+
+  def create
+    @sent_message.save
+=begin respond_to do |format|
+                #	format.json { User.create(user_params); render :json => User.last.to_json}
+                #	format.html { @user = User.new(user_params)
+                                if @user.save
+                                        redirect_to users_path
+                                else
+                                        render 'new'
+                                end }
+=end
+
   end
 end
