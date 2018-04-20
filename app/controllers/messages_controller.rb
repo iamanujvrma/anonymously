24e8b283ae7c1fabd80616c28a3826a00dbb6fe0
dev <mailakshayb1@gmail.com>
2018-04-20 21:49:41 +0530

    [akshay]messages bugs fixed

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
+  before_action :authenticate_user!
   def index
   end
   
@@ -7,15 +8,20 @@ class MessagesController < ApplicationController
   end
 
   def create
+    @message = current_user.sent_messages.new
     m1 = message_params
     @email = m1[:receiver_id]
-    m1[:receiver_id] = User.find_by_email(@email).id
-    @message = current_user.sent_messages.new(m1)
-    @message.save!
-    if @message.save!
-      redirect_to dashboard_index_path
+    if User.exists?(email: @email)
+      m1[:receiver_id] = User.find_by_email(@email).id
+      @message = current_user.sent_messages.new(m1)
+      @message.save!
+      if @message.save!
+        redirect_to dashboard_index_path
+      else
+        redirect_to new_message_path(current_user)
+      end
     else
-      redirect_to new_message_path(current_user)
+      @message.errors.add(:receiver_id,"does not exists.")
     end
   end
 
