a584bd330991ed9343dc9309eeb6b4038b11e75b
dev <mailakshayb1@gmail.com>
2018-04-22 15:20:22 +0530

    [akshay]adds condition that receiver must exists

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,11 +9,15 @@ class MessagesController < ApplicationController
   def create
     m1 = message_params
     @email = m1[:receiver_id]
-    m1[:receiver_id] = User.find_by_email(@email).id
-    @message = current_user.sent_messages.new(m1)
-    @message.save!
-    if @message.save!
-      redirect_to dashboard_index_path
+    if User.exists?(email: @email)
+      m1[:receiver_id] = User.find_by_email(@email).id
+      @message = current_user.sent_messages.new(m1)
+      @message.save!
+      if @message.save!
+        redirect_to dashboard_index_path
+      else
+        redirect_to new_message_path(current_user)
+      end
     else
       redirect_to new_message_path(current_user)
     end
