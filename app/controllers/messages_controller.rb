efd636db6861f38dedb8fa7d172283f2e9190676
dev <mailakshayb1@gmail.com>
2018-04-23 16:38:15 +0530

    [akshay]used query optimization

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,10 +9,10 @@ class MessagesController < ApplicationController
   def create
     m1 = message_params
     @email = m1[:receiver_id]
-    if User.exists?(email: @email)
-      m1[:receiver_id] = User.find_by_email(@email).id
+    @receiver = User.find_by_email(@email)
+    if @receiver != nil
+      m1[:receiver_id] = @receiver.id
       @message = current_user.sent_messages.new(m1)
-      @message.save!
       if @message.save!
         redirect_to dashboard_index_path
       else
