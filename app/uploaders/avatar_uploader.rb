1be2c56cdb477bc6e67360854a501af62fdcd5c7
dev <mailakshayb1@gmail.com>
2018-04-26 19:02:41 +0530

    [akshay]adds medium image resize

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -14,12 +14,12 @@ class AvatarUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url(*args)
+    # For Rails 3.1+ asset pipeline compatibility:
+    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default-dp.jpg"].compact.join('_'))
+  
+    # "/images/fallback/" + [version_name, "default-dp.jpg"].compact.join('_')
+  end
   #process resize_to_fill: [600, 0]
   # Process files as they are uploaded:
   # process scale: [200, 300]
@@ -32,6 +32,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
    version :thumb do
     process resize_to_fit: [50, 50]
    end
+  version :medium do
+    process :resize_to_fill => [150, 150]
+  end
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   #def extension_whitelist
