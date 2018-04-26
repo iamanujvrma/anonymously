52b8aeaf3b45a784f373ac225e9d3d733ef4ed1e
dev <mailakshayb1@gmail.com>
2018-04-26 13:31:46 +0530

    [akshay]Singup in homepage with devise errors

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  helper_method :resource_name, :resource, :devise_mapping, :resource_class
+  
   def index
   end
 end
